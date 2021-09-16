195e37760aab0e4c6d456d6d3a8e7b9ab8188981
Cavid <hcavid386@gmail.com>
2021-09-16 14:20:19 +0400

    Update Skill.java

--- JavaDbApp/src/main/java/Bean/Skill.java
+++ JavaDbApp/src/main/java/Bean/Skill.java
@@ -3,5 +3,28 @@ package Bean;
 public class Skill {
     private int id;
     private String name;
-    
+
+    public Skill() {
+    }
+
+    public Skill(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
